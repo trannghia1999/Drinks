b42e6f99ceddabe0f2991e839e35668c22c6eb15
unknown <nghiabe1510@gmail.com>
2021-05-23 22:18:49 +0700

    uploadmore

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import CocktailList from '../components/CocktailList'
+import SearchForm from "../components/SearchForm"
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+     <>
+     <SearchForm/>
+     <CocktailList/>
+     </>
     )
 }
 
